12b3eefc41fcec4f5e0151c81325ea6a77ecef68
neo <neorepo@gmail.com>
2020-09-08 19:27:13 -0300

    detalles menores

--- db/test.sql
+++ db/test.sql
@@ -3,8 +3,7 @@ INSERT INTO "asociado" VALUES (2,'Orosco','María Victoria','F','1991-10-28','DN
 INSERT INTO "asociado" VALUES (3,'Capelli','Sabina Ximena','F','1986-01-08','DNI','32122871','27321228718','Adherente','sabina.capelli@gmail.com','Manuel Antonio Sáez 1253 B° Mateo Vidal',5663,'2020-07-06 16:03:21','2020-08-22 04:05:09',0);
 INSERT INTO "asociado" VALUES (4,'Pomahuallca C.','Rosario Estheysi','F','1990-04-16','DNI','94126317','27941263173','Activo','rosario.china@hotmail.com','Brandsen 1997',16441,'2020-07-06 16:05:37','2020-08-27 22:24:47',0);
 INSERT INTO "asociado" VALUES (5,'Doe','Jeff','M','2000-03-03','DNI','33312345','20333123453','Activo','neo@emailna.co','J. Rodriguez 296',5495,'2020-07-07 21:35:32','2020-08-27 01:06:12',0);
-INSERT INTO "asociado" VALUES (6,'Y. Silva','Julio Cesar','M','1981-03-03','DNI','94269698','20942696982','Activo','neo.code.edu@gmail.com','B° El Progreso Manz. B Ca.7',5495,'2020-08-22 11:40:39','2020-08-27 22:29:43',0);
-INSERT INTO "asociado" VALUES (7,'Estrella','Romina','F','1993-08-05','DNI','37811852','27378118528','Activo','romina_ayelen93@hotmail.com','Lisandro Moyano 2974',5663,'2020-08-22 23:06:25','2020-08-27 00:29:04',0);
+INSERT INTO "asociado" VALUES (7,'Estrella','Romina Ayelén','F','1993-08-05','DNI','37811852','27378118528','Activo','romina_ayelen93@hotmail.com','Lisandro Moyano 2974',5663,'2020-08-22 23:06:25','2020-08-27 00:29:04',0);
 INSERT INTO "asociado" VALUES (8,'Barrera Gloria','Silvina Edit','F','1969-06-07','DNI','20810218','23208102184','Activo',NULL,'Desconocido',5495,'2020-08-26 21:41:28','2020-08-27 22:28:49',0);
 INSERT INTO "asociado" VALUES (9,'Rufino','Samir Abel','M','2001-01-01','DNI','35552636','20355526365','Activo',NULL,'Desconocido',5800,'2020-08-27 00:00:30','2020-08-27 00:28:12',0);
 
@@ -13,7 +12,6 @@ INSERT INTO "telefono" VALUES (2,'2634762526',NULL,2,'2020-07-06 16:00:35','2020
 INSERT INTO "telefono" VALUES (3,'2612697826','2614308275',3,'2020-07-06 16:03:21','2020-08-22 04:05:09');
 INSERT INTO "telefono" VALUES (4,'1164441066',NULL,4,'2020-07-06 16:05:37','2020-08-27 22:24:47');
 INSERT INTO "telefono" VALUES (5,'2617584789',NULL,5,'2020-07-07 21:35:32','2020-08-27 01:06:12');
-INSERT INTO "telefono" VALUES (6,'2615161142','2614200192',6,'2020-08-22 11:40:39','2020-08-27 22:29:43');
 INSERT INTO "telefono" VALUES (7,'2613387401',NULL,7,'2020-08-22 23:06:25','2020-08-27 00:29:04');
 INSERT INTO "telefono" VALUES (8,'2616928862',NULL,8,'2020-08-26 21:41:28','2020-08-27 22:28:49');
 INSERT INTO "telefono" VALUES (9,'2615570805',NULL,9,'2020-08-27 00:00:30','2020-08-27 00:28:12');
